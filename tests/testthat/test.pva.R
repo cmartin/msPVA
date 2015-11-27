493271a5a5d8600168d4c8157a2edf406ac897e7
Charles Martin <martin.charles@gmail.com>
2015-11-27 14:34:06 -0500

    Completely rewritten tests so that they work properly

--- tests/testthat/test.pva.R
+++ tests/testthat/test.pva.R
@@ -1,5 +1,3 @@
-context("Verifying agreement with the MatLab code")
-
 res <- simulate_ms_pva(
  leaving_prob = 0.2,
  reaching_prob = 0.5,
@@ -15,67 +13,73 @@ res <- simulate_ms_pva(
    byrow = TRUE
  )},
  K = c(286, 60, 58),
- quasi_extinction_thresholds = c(20, 20, 20)
+ quasi_extinction_thresholds = c(20, 20, 20),
+ with_progress_bar = FALSE
 )
 
-all.equal(
-  res$lam0,
-  1.0320,
-  tolerance = .001
-)
+test_that("Verifying agreement with the MatLab code for Multi-site code",{
 
-all.equal(
-  mean(res$stochLam),
-  0.99875,
-  tolerance = .001
-)
+  expect_equal(
+    res$lam0,
+    1.0320,
+    tolerance = .001
+  )
 
-all.equal(
-  res$lam0,
-  1.0320,
-  tolerance = .001
-)
+  expect_equal(
+    mean(res$stochLam),
+    0.99875,
+    tolerance = .001
+  )
 
-all.equal(
-  mean(res$logLam),
-  -0.0012787,
-  tolerance = .005
-)
+  expect_equal(
+    mean(res$logLam),
+    -0.0012787,
+    tolerance = .005
+  )
 
-all.equal(
-  sd(res$logLam),
-  0.0070218,
-  tolerance = .005
-)
+  expect_equal(
+    sd(res$logLam),
+    0.0070218,
+    tolerance = .01
+  )
 
-context("Parameter loading from file")
 
-params <- calculate_params_from_file(
-  system.file("extdata", "PolarBear_Stirling2004.csv", package = "msPVA")
-)
+})
 
-all.equal(
-  c(params$growth_rate_corrs),
-  c(1.0000000, -0.6207679, -0.6207679,  1.0000000),
-  tolerance = .0001
-)
+test_that("Parameter loading from file",{
 
-all.equal(
-  params$initial_pops,
-  c(201, 140),
-  check.attributes = FALSE
-)
+  expect_warning(
+    params <- calculate_params_from_file(
+      system.file("extdata", "PolarBear_Stirling2004.csv", package = "msPVA")
+    )
+  )
 
-all.equal(
-  params$growth_rate_means,
-  c( 0.02362944, 0.01572059),
-  tolerance = .0001,
-  check.attributes = FALSE
-)
+  expect_equal(
+    c(params$growth_rate_corrs),
+    c(1.0000000, -0.6207679, -0.6207679,  1.0000000),
+    tolerance = .0001
+  )
 
-all.equal(
-  params$growth_rate_vars,
-  c(0.1292640, 0.1639641),
-  tolerance = .0001,
-  check.attributes = FALSE
-)
+  expect_equal(
+    params$initial_pops,
+    c(201, 140),
+    check.attributes = FALSE
+  )
+
+  attributes(params$growth_rate_means) <- NULL
+
+  expect_equal(
+    params$growth_rate_means,
+    c(0.02362944, 0.01572059),
+    tolerance = .001
+  )
+
+  attributes(params$growth_rate_vars) <- NULL
+
+  expect_equal(
+    params$growth_rate_vars,
+    c(0.1292640, 0.1639641),
+    tolerance = .0001
+  )
+
+})
